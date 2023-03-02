fc9c5a1348ee3d35e5c6cf4f353afe87182bfecc
Cody Wallbridge <codywallbridge@gmail.com>
2023-03-01 22:25:03 -0600

    boiler plate code implemented

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 4380Project
+
+## Operating Instructions:
+Results will be output to output.txt. If you want to reproduce the results, install python, and run "python .\main.py". Results will be generated and written to output.txt where they can be viewed.
+
+## Things to know
+DB file gets automatically created and cleaned up with the program so it is a completely fresh run each time. Due to size of data, this can result in some delay during startup but the timings are only taken when executing queries.
--- main.py
+++ main.py
@@ -1,41 +1,80 @@
 import sqlite3
+import csv
+import datetime
+import os
+import time
 
-def createFiles():
-    cursor.execute("CREATE TABLE movie(title, year, score)")
+
+def createTables():
+    cursor.execute("CREATE TABLE names(id, name, word)")
+    cursor.execute("CREATE TABLE table1(id, letter, newWord)")
     con.commit()
 
 
-def insertIntoFiles():
-    cursor.execute("""
-    INSERT INTO movie VALUES
-        ('Monty Python and the Holy Grail', 1975, 8.2),
-        ('And Now for Something Completely Different', 1971, 7.5)
-""")
-    con.commit()
+def insertIntoTables():
+    with open('names.csv','r') as file:
+        namesReader = csv.DictReader(file) # comma is default delimiter
+        to_db = [(i['Id'], i['Name'], i['Word']) for i in namesReader]
+
+        cursor.executemany("INSERT INTO names (Id, Name, Word) VALUES (?, ?, ?);", to_db)
+        con.commit()
+
+
+    with open('table1.csv','r') as file:
+        table1Reader = csv.DictReader(file) # comma is default delimiter
+        to_db = [(i['Id'], i['letter'], i['newWord']) for i in table1Reader]
+
+        cursor.executemany("INSERT INTO table1 (Id, letter, newWord) VALUES (?, ?, ?);", to_db)
+        con.commit()
+
 
 def runQueries():
-    result = cursor.execute("SELECT score FROM movie")
-    result.fetchall()
+    query = "SELECT * FROM names ORDER BY Id LIMIT 5;"
+    start = datetime.datetime.now()
+    result = cursor.execute(query)
+    time.sleep(5)#can be removed. just added artificial timing to it.
+    end = datetime.datetime.now()
+    queryResult = result.fetchall()
+    time_elapsed = end - start
+    file.write("Current query is: \"" + query + "\"\n")
+    file.write("Time elapsed for the query is " + str(time_elapsed) + ":\n")
+    #print("Time elapsed is " + str(time_elapsed) + ":")
+    #print("names result:")
+    for row in queryResult:
+        file.write(str(row) + "\n")
+        #print(row)
+
+    file.write("\n")
+
+    query = "SELECT * FROM table1 ORDER BY Id LIMIT 5;"
+    start = datetime.datetime.now()
+    result = cursor.execute(query)
+    time.sleep(5)#can be removed. just added artificial timing to it.
+    end = datetime.datetime.now()
+    queryResult = result.fetchall()
+    time_elapsed = end - start
+    file.write("Current query is: \"" + query + "\"\n")
+    file.write("Time elapsed for the query is " + str(time_elapsed) + ":\n")
+    #print("Time elapsed is " + str(time_elapsed) + ":")
+    #print("table1:")
+    for row in queryResult:
+        file.write(str(row) + "\n")
+        #print(row)
 
 
 
 
 con = sqlite3.connect("amazonReviews4380.db")
-createFiles()
-insertIntoFiles()
-runQueries()
-
-
 cursor = con.cursor()
-result = cursor.execute("SELECT ....") # run whatever query you want
-cursor.execute("UPDATE ....") # update whatever table you want
-con.commit() # commits are necessary for every change
-
-# BATCH COMMIT 
-data = [
-    ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
-    ("Monty Python's The Meaning of Life", 1983, 7.5),
-    ("Monty Python's Life of Brian", 1979, 8.0),
-]
-cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
-con.commit()  # Remember to commit the transaction after executing INSERT.
+file = open("output.txt", "w")
+file.write("Database Query Results:\n\n")
+createTables()
+insertIntoTables()
+runQueries()
+con.close()
+if os.path.exists("amazonReviews4380.db"):
+    # remove the database file
+    os.remove("amazonReviews4380.db")
+else:
+    print(f"The file amazonReviews4380.db does not exist.")
+print("Done executing")
